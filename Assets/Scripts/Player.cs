0c9d42cece40b2f330b81780a7c572cd8dea9ef0
jonathan-farah <jonathanfarah05@gmail.com>
2023-02-24 16:32:41 -0500

    started movement system

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -5,14 +5,25 @@ using UnityEngine;
 public class Player : MonoBehaviour
 {
     // Start is called before the first frame update
+    [SerializeField]
+     private Vector2 movementDirection;
+     private Rigidbody2D rb;
+     private float moveSpeed; 
+
     void Start()
     {
-        
+        rb = GetComponent<Rigidbody2D>();
+        moveSpeed = 5.0f;
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+     movementDirection = new Vector2(Input.GetAxis("Horizontal"),Input.GetAxis("Vertictal"));
+
+    }
+
+    void FixedUpdate(){
+        rb.velocity = movementDirection*moveSpeed;
     }
 }
